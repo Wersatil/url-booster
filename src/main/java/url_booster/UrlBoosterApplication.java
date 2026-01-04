7f7572e08f1db61ac0899709efd88b006567e162
Wersatil <159185890+Wersatil@users.noreply.github.com>
2026-01-04 10:15:41 +0300

    Add files via upload

---
+++ src/main/java/url_booster/UrlBoosterApplication.java
@@ -0,0 +1,13 @@
+package url_booster;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UrlBoosterApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UrlBoosterApplication.class, args);
+	}
+
+}
---
+++ src/test/java/url_booster/UrlBoosterApplicationTests.java
@@ -0,0 +1,13 @@
+package url_booster;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UrlBoosterApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
